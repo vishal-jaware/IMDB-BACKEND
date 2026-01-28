9b5260d076b7397d0512c3071cbd583f7fa640df
vishal-jaware <vishaljaware10@gmail.com>
2026-01-28 15:54:19 +0530

    again cors updated

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://imdb-front-end.netlify.app"
+    "https://imdbfront-end.netlify.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"]
